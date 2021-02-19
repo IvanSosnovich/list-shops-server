32799d9cccd07d9a866cc991cd66a331a6372f44
Ivan Sosnovich <ivan.sosnovich1987@gmail.com>
2021-02-19 15:24:54 +0300

    fix: add cors

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const { connect, connection } = require('./model/main');
-require('dotenv').config();
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const { connect, connection } = require("./model/main");
+require("dotenv").config();
 
 const app = express();
 
-const userRouter = require('./router/userRouter');
+const userRouter = require("./router/userRouter");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const sessionConfig = {
   store: new MongoStore({ mongooseConnection: connection }),
-  key: 'sid',
-  secret: 'example',
+  key: "sid",
+  secret: "example",
   resave: true,
   saveUninitialized: false,
   cookie: { expires: 600000 },
@@ -23,15 +23,25 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 
-app.use('/user', userRouter);
+const corsMiddleware = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+};
+
+app.use(corsMiddleware);
+app.use("/user", userRouter);
 
 app.listen(process.env.PORT, () => {
-  console.log('server start');
+  console.log("server start");
   connect(
     process.env.DB_URL,
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
     () => {
-      console.log('base start');
+      console.log("base start");
     },
   );
 });
